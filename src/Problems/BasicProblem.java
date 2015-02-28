226c6b62ecac7991e5d49d4d5c04ed7b867b0d9c
albert <Albert.Volschenk@gmail.com>
2015-02-28 17:28:37 +0200

    Fixed BasicProblem functions

--- src/Problems/BasicProblem.java
+++ src/Problems/BasicProblem.java
@@ -26,8 +26,7 @@ public class BasicProblem extends Problem {
             double x = solutionVector[0];
 
             //Calculate the result of the function when you plug in the variables
-            Double result = 30.0d + (25.0d * x) - (4.0d * Math.pow(x, 2.0d)) + (0.15d * Math.pow(x, 3.0d));
-            return result;
+            return 30.0d + (25.0d * x) - (4.0d * Math.pow(x, 2.0d)) + (0.15d * Math.pow(x, 3.0d));
         } else
             throw new Exception("Invalid input variables for problem.");
     }
